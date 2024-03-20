53e839b0684ce4ea3329561efacac0e1cfde2267
Will Taylor <will@wills.network>
2024-03-20 02:56:18 -0500

    Added lsp to lazy vim

--- nvim/lua/kalzan/remap.lua
+++ nvim/lua/kalzan/remap.lua
@@ -1,1 +1,8 @@
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.wo.number = true
+vim.wo.relativenumber = true
