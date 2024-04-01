f3ad8413c1c6ac3cdf056ba86a7d90ddfec360a4
Will Taylor <will@wills.network>
2024-03-31 21:46:11 -0500

    added fish stuff

---
+++ fish/functions/cdr.fish
@@ -0,0 +1,4 @@
+function cdr --wraps='cd ~/Documents/rust' --description 'alias cdr=cd ~/Documents/rust'
+  cd ~/Documents/rust $argv
+        
+end
---
+++ fish/functions/cdvim.fish
@@ -0,0 +1,4 @@
+function cdvim --wraps='cd ~/.config/nvim/lua/kalzan' --description 'alias cdvim=cd ~/.config/nvim/lua/kalzan'
+  cd ~/.config/nvim/lua/kalzan $argv
+        
+end
